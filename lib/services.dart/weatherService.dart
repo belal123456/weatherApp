2872179dbac0f3f2cb6ed53c56575ca9a1bb8b73
belal_soliman <belalsoliman8@gmail.com>
2024-07-29 01:49:43 +0300

    extract base url

--- lib/services.dart/weatherService.dart
+++ lib/services.dart/weatherService.dart
@@ -3,9 +3,10 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class Weatherservice {
+  String baseUrl = "http://api.weatherapi.com/v1";
+  String ApiKey = 'c32a475a3b564ebc914230358242307&q';
   getWeather({required String cityName}) async {
-    Uri url = Uri.parse(
-        "http://api.weatherapi.com/v1/current.json?key=c32a475a3b564ebc914230358242307&q=London&aqi=no");
+    Uri url = Uri.parse("$baseUrl/.json?key=$ApiKey=$cityName&aqi=no");
     http.Response response = await http.get(url);
     Map<String, dynamic> data = jsonDecode(response.body);
   }
